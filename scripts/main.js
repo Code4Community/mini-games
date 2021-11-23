cbe1dfb7192d3a0d4df77bba3afbe64e59300c0a
akundla <alekkundla@gmail.com>
2021-11-22 20:12:23 -0500

    Show question when star is clicked
    Co-authored-by: kateherz <kateherz@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,124 @@
+// https://labs.phaser.io/edit.html?src=src/scenes/scene%20injection%20map.js&v=3.55.2
+var MyScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function MyScene ()
+    {
+        //  Here we'll tailor the property injection map so that by default our Scene
+        //  only gets 2 properties defined on it: 'makeStuff' and 'loader'.
+        //  It will also have the property 'sys' which can never be redefined or removed.
+        var config = {
+            map: {
+                add: 'add',
+                load: 'load',
+                input: 'input'
+            }
+        };
+
+        Phaser.Scene.call(this, config)
+    },
+
+    /**
+     * Preload function required by Phaser
+     * loads assets
+     */
+    preload: function ()
+    {
+        this.load.image('star', '../assets/star.png');
+        this.load.html("form", "../templates/form.html");
+        this.load.image('bg', '../assets/space.jpeg');
+    },
+
+    /**
+     * Create function required by Phaser
+     * draws everything we want on the screen
+     * Sets up responsive actions
+     */
+    create: function ()
+    {
+        target = this.add.image(150, 150, "star");
+        target.angle = 25;
+        target.setInteractive();
+
+        //this will listen for a down event
+        //on every object that is set interactive
+        this.input.on('gameobjectdown', this.onObjectClicked);
+
+        var score = 4;
+        var scoreText;
+        scoreText = this.add.text(600, 50, 'score:' + score, { fontSize: '32px', fill: '#FFFFFF' });
+        
+        //this.add.image(400, 300, 'bg');
+
+    
+        this.nameInput = this.add.dom(640, 360).createFromCache("form");
+
+        this.message = this.add.text(640, 250, "Hello, --", {
+            color: "#FFFFFF",
+            fontSize: 60,
+            fontStyle: "bold"
+        }).setOrigin(0.5);
+
+        this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
+        this.returnKey.on("down", event => {
+            let name = this.nameInput.getChildByName("name");
+            if (name.value != "") {
+                this.message.setText("Hello, " + name.value);
+                name.value = "";
+            }
+        });
+    },
+
+    /**
+     * Update function required by Phaser
+     * Runs repeatedly on a cycle
+     */
+    update: function () 
+    {
+        var x, y;
+        if (game.input.mousePointer.isDown) {
+            x = game.input.mousePointer.x;
+            y = game.input.mousePointer.y
+            console.log(x, y);
+        }
+    },
+    
+    question: [
+        "Disney Quiz:",
+        "",
+        "if (mickey's sister == Minnie)",
+        "",
+        "(User enter input)"
+    ],
+    
+    showQuestion: function ()
+    {
+        var r1 = this.add.rectangle(400, 150, 300, 200, 0x3c3c3f);
+        var text = this.add.text(
+            300,
+            100,
+            this.question,
+            {
+                fontFamily: 'Arial', color: '#00ff00', wordWrap: {
+                    width: 500
+                }
+            }
+        ).setOrigin(0);
+    },
+
+    onObjectClicked: function(object) 
+    {
+        // Rotate the object
+        object.angle+=10;
+        // Pop up the question
+        this.scene.showQuestion();
+    }
+});
+
 // Phaser configuration object
 let config = {
     type: Phaser.AUTO,
@@ -14,110 +135,8 @@ let config = {
             debug: false,
         },
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update,
-    },
+    scene: MyScene
 };
 
 // Make the basic game with the config file
 var game = new Phaser.Game(config);
-
-// Target to hit, global var
-let target;
-
-/**
- * Preload function required by Phaser
- * loads assets
- */
-function preload() {
-    this.load.image('star', '../assets/star.png');
-    this.load.html("form", "../templates/form.html");
-    this.load.image('bg', '../assets/space.jpeg');
-}
-
-/**
- * Create function required by Phaser
- * draws everything we want on the screen
- * Sets up responsive actions
- */
-function create() {
-    // this is an example of phaser.scene
-
-    target = this.add.image(150, 150, "star");
-    target.angle = 25;
-    target.setInteractive();
-
-    //this will listen for a down event
-    //on every object that is set interactive
-    this.input.on('gameobjectdown', onObjectClicked, this);
-
-    var score = 4;
-    var scoreText;
-    scoreText = this.add.text(600, 50, 'score:' + score, { fontSize: '32px', fill: '#FFFFFF' });
-    
-    //this.add.image(400, 300, 'bg');
-
-    var question1 = [
-        "Disney Quiz:",
-        "",
-        "if (mickey's sister == Minnie)",
-        "",
-        "(User enter input)"
-    ];
-    this.nameInput = this.add.dom(640, 360).createFromCache("form");
-
-    this.message = this.add.text(640, 250, "Hello, --", {
-        color: "#FFFFFF",
-        fontSize: 60,
-        fontStyle: "bold"
-    }).setOrigin(0.5);
-
-    this.returnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-
-    this.returnKey.on("down", event => {
-        let name = this.nameInput.getChildByName("name");
-        if (name.value != "") {
-            this.message.setText("Hello, " + name.value);
-            name.value = "";
-        }
-    });
-}
-
-/**
- * Update function required by Phaser
- * Runs repeatedly on a cycle
- */
-function update() {
-    var x, y;
-    if (game.input.mousePointer.isDown) {
-        x = game.input.mousePointer.x;
-        y = game.input.mousePointer.y
-        console.log(x, y);
-    }
-}
-
-function showQuestion() {
-    console.log(scene)
-    var r1 = scene.add.rectangle(400, 150, 300, 200, 0x3c3c3f);
-    var text = scene.add.text(
-        300,
-        100,
-        question1,
-        {
-            fontFamily: 'Arial', color: '#00ff00', wordWrap: {
-                width: 500
-            }
-        }
-    ).setOrigin(0);
-}
-
-function onObjectClicked(pointer, object, thirdParam) {
-    // Rotate the object
-    object.angle+=10;
-    // testing
-    console.log(thirdParam);
-    // Pop up the question
-    showQuestion();
-}
