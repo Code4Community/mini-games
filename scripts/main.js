9082e87b825c6707324f9de968df3e3be269aa7e
Raphael <sargsyanraph@gmail.com>
2022-03-07 20:30:49 -0500

    added some questions

--- scripts/main.js
+++ scripts/main.js
@@ -129,7 +129,7 @@ var MyScene = new Phaser.Class({
     // TODO: Create a question JS constructor
     questionList: [
         
-/* 
+
         //Frozen themed questions
         new Question("if(Elsas Sister == Ana)", true),
         new Question("if(Olafs favorite season == summer)", true),
@@ -141,7 +141,7 @@ var MyScene = new Phaser.Class({
         new Question("if(Olaf has a nose && Olaf doesnt have eyebrows)", false), // Second part false
         new Question("if(There are 6 spirits && Elsa is the fifth spirit)", false), // First part false
         //add or questions, two true, first one true, second one true, both false
-        */
+        
 
 
         //Sports Themed questions
@@ -150,6 +150,14 @@ var MyScene = new Phaser.Class({
         new Question("CJ Stroud is the starting quarterback for the Buckeyes OR the Cleveland Browns went to the Playoffs", true),
         new Question("Columbus has a football team in the NFL", false),
         new Question("Columbus has a hockey team in the NHL", true),
+        new Question("IF the Columbus Blue Jackets win the Semi Finals, they go to the Super Bowl", false),
+        new Question("If the Buckeyes score a touchdown, they will gain 6 points", true),
+
+
+
+
+ 
+
 
 
     ],
